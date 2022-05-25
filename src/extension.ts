f001cd22632f112f0f350258acda438b1e085986
Gapur Kassym <gapur.kassym@gmail.com>
2022-05-25 21:29:35 +0300

    feat(package): change activationEvents

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,7 @@ import * as vscode from "vscode";
 import tokens from "./tokens";
 import { Token } from "./models/token";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log(
     'Congratulations, your extension "twilio-paste-intellisense" is now active!'
   );
@@ -50,5 +46,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(hover);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
